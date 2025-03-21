e855c1b03dfa6b465049e88e562422a42cb1abcd
Son Nguyen <sonnh@avadagroup.com>
2025-03-21 15:59:08 +0700

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,21 @@
-const Encore = require('@symfony/webpack-encore');
+const Encore = require("@symfony/webpack-encore");
 
-// Đặt thư mục đầu ra thành /assets/build 
-// Đường dẫn này sẽ được Jekyll truy cập
-Encore
-    // Thư mục đầu ra
-    .setOutputPath('assets/')
-    
-    // Public path sử dụng trong browser
-    .setPublicPath('/assets')
-    
-    // Thêm entry cho CSS
-    .addStyleEntry('css/app', './core/css/app.css')
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev");
+}
 
-    // Chỉ cần một entry cho mỗi trang JavaScript
-    .addEntry('js/app', './core/js/app.js')
-    
-    // Bật SourceMaps cho development
-    .enableSourceMaps(!Encore.isProduction())
-    
-    // Tắt các feature bạn không sử dụng
+process.env.NODE_ENV = Encore.isProduction() ? "production" : "dev";
+
+Encore.setOutputPath("assets/")
+    .setPublicPath("/assets")
+    .addStyleEntry("css/app", "./core/css/app.css")
+    .addEntry("js/app", "./core/js/app.js")
+    .enablePostCssLoader()
     .disableSingleRuntimeChunk()
-    
-    // Xóa các file cũ trước khi tạo file mới
-    .cleanupOutputBeforeBuild()
-    
-    // Thêm thông báo build
-    .enableBuildNotifications()
-    
-    // Cấu hình Babel
-    .configureBabel((config) => {
-        config.presets.push('@babel/preset-env');
-    })
-;
+    .enableSourceMaps(!Encore.isProduction())
+    .configureBabel(() => {}, {
+        useBuiltIns: "usage",
+        corejs: 3,
+    });
 
 module.exports = Encore.getWebpackConfig();
