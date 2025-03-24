9d479bf9105bb4627a785422ff2517768c5af8c7
Son Nguyen <sonnh@avadagroup.com>
2025-03-24 09:31:11 +0700

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,28 @@
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./_includes/**/*.{html,js}",
-    "./_layouts/**/*.{html,js}",
-    "./_pages/**/*.{html,md}",
-    "./index.html",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#001489", // Dark blue from image
-        secondary: "#0cb6e9", // Light blue from image
-        accent: "#ff5733", // Orange color for accents
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
+    darkMode: "class",
+    content: [
+        "./_includes/**/*.{html,js}",
+        "./_layouts/**/*.{html,js}",
+        "./_pages/**/*.{html,md}",
+        "./index.html",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#001489", // Dark blue from image
+                secondary: "#0cb6e9", // Light blue from image
+                accent: "#ff5733", // Orange color for accents
+            },
+            fontFamily: {
+                sans: ["Inter", "sans-serif"],
+            },
+            width: {
+                82: "82%",
+            },
+            padding: {
+                15: "4rem", // 72px (nếu 1rem = 16px)
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
